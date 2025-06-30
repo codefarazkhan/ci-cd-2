1bfb719b0244b687e5cef126c3f08a3d10f6ad66
codefarazkhan <code2.farazkhan@gmail.com>
2025-06-30 12:34:30 +0530

    fix eslint

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-var test = 'test';
 
 // Define a route
 app.get('/', (req, res) => {
-    // Log the request to the consol
-          console.log('Received a request at /');
-    console.log('Received a request at /2');
-    console.log('Received a request at /3');
-    console.log('Received a request at /4');
-    console.log('Received a request at /5');
-    console.log('Received a request at /6ss');
+  // Log the request to the console
+  console.log('Received a request at /');
+  console.log('Received a request at /2');
+  console.log('Received a request at /3');
+  console.log('Received a request at /4');
+  console.log('Received a request at /5');
+  console.log('Received a request at /6ss');
+
   res.send('Hello World!');
 });
 
