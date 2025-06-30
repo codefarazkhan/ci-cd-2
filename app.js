acf56c9a3a1529925dd2cc5c8d89ad7a556e99fb
codefarazkhan <code2.farazkhan@gmail.com>
2025-06-30 12:53:26 +0530

    done

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 // Define a route
 app.get('/', (req, res) => {
   // Log the request to the console
-   console.log('Received a request at /');
+  console.log('Received a request at /');
   console.log('Received a request at /2');
   console.log('Received a request at /3');
   console.log('Received a request at /4');
