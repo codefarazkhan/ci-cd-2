dd05f478cb4f8c9751e398a792307f4e917d9995
codefarazkhan <code2.farazkhan@gmail.com>
2025-06-30 12:32:46 +0530

    check lint

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+var test = 'test';
 
 // Define a route
 app.get('/', (req, res) => {
-    console.log('Received a request at /');
+    // Log the request to the consol
+          console.log('Received a request at /');
     console.log('Received a request at /2');
     console.log('Received a request at /3');
     console.log('Received a request at /4');
     console.log('Received a request at /5');
-    console.log('Received a request at /6');
+    console.log('Received a request at /6ss');
   res.send('Hello World!');
 });
 
