3eabfe04ad79b6895b8556b7ffe083df3a7727fa
codefarazkhan <code2.farazkhan@gmail.com>
2025-06-30 12:29:57 +0530

    done 22

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
     console.log('Received a request at /3');
     console.log('Received a request at /4');
     console.log('Received a request at /5');
+    console.log('Received a request at /6');
   res.send('Hello World!');
 });
 
